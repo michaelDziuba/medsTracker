5b4cfde9a56f338380e49fd2e5929564e2aa049d
Michael Dziuba <michael.dziuba@stclairconnect.ca>
2017-03-28 23:42:17 -0400

    #2 ISSUE: Added custom Camera to the app for adding pictures.

---
+++ MedsTracker2/app/src/main/java/com/example/cdu/medstracker/AddPhotoFragment.java
@@ -0,0 +1,465 @@
+package com.example.cdu.medstracker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.PorterDuff;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_OK;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link AddPhotoFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link AddPhotoFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AddPhotoFragment extends Fragment {
+
+
+    public static String photoPath =  null;
+    public static int selectedItem = 0;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private static final int REQUEST_CODE = 0x11;
+    String[] permissions = {"android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE", "android.permission.CAMERA"};
+
+
+    FloatingActionButton fab = MainActivity.fab;
+    Spinner spin;
+    ImageView cameraButton;
+    LinearLayout galleryLayout;
+    ArrayList<Drug> bookList;
+
+    private static final int CAMERA_INTENT = 1;
+    private String imageLocation;
+
+    private OnFragmentInteractionListener mListener;
+
+    public AddPhotoFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AddPhotoFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AddPhotoFragment newInstance(String param1, String param2) {
+        AddPhotoFragment fragment = new AddPhotoFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_photo, container, false);
+
+
+        //Asks the user for permission to access photos, media, and files on the user's device
+        ActivityCompat.requestPermissions(getActivity(), permissions, REQUEST_CODE);
+
+        if(fab.isShown()){
+            fab.setVisibility(View.INVISIBLE);
+        }
+
+//        Button takePhotoButton = (Button) view.findViewById(R.id.takePhoto);
+//        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ((MainActivity)getActivity()).takePhotos();
+//            }
+//        });
+
+        galleryLayout = (LinearLayout) view.findViewById(R.id.galleryLayout);
+
+
+        if(photoPath != null){
+            ImageView imageView = new ImageView(getContext());
+            imageView.setMaxHeight((int)(MainActivity.screenWidth * 0.8));
+            imageView.setScaleType(ImageView.ScaleType.FIT_START);
+            imageView.setAdjustViewBounds(true);
+            //imageView.setImageBitmap(scaled);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(photoPath));
+            imageView.setBackgroundColor(Color.LTGRAY);
+            imageView.setPadding(0,15,0,15);
+            galleryLayout.addView(imageView);
+        }
+
+
+        spin = (Spinner) view.findViewById(R.id.bookSpinner);
+
+
+        DatabaseHandler db = new DatabaseHandler(getContext());
+
+        bookList = db.getAllBooks();
+
+        //db.closeDB();
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_dropdown_item, bookList);
+        adapter.setDropDownViewResource(R.layout.spinner_item_layout);
+        spin.setAdapter(adapter);
+        spin.setSelection(selectedItem);
+
+
+        Drug book = (Drug) spin.getSelectedItem();
+        if(book == null){
+            Toast toast = Toast.makeText(getActivity(), "Please add a drug,\nbefore adding a photo", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL, 0, 0);
+            toast.show();
+        }
+
+        if(photoPath != null){
+            Image image = new Image(book.getId(), photoPath);
+
+               /**
+                * Add the photo to the database
+                */
+               //DatabaseHandler db = new DatabaseHandler(getContext());
+               int id = (int) db.addImage(image);
+               if (id != -1) {
+                   image.setId(id);
+                   Toast.makeText(getActivity(), "Photo Added", Toast.LENGTH_LONG).show();
+               } else {
+                   Toast.makeText(getActivity(), "Photo Not Added", Toast.LENGTH_LONG).show();
+               }
+        }
+
+        db.closeDB();
+
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position != selectedItem) {
+                    galleryLayout.removeAllViews();
+                }
+//                TextView textView = (TextView) parent.getSelectedView();
+//                if(textView != null) {
+//                    textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+//                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //parent.setSelection(0);
+            }
+        });
+
+
+
+        cameraButton = (ImageView) view.findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectedItem = spin.getSelectedItemPosition();
+
+                ((MainActivity)getActivity()).takePhotos();
+
+//                File image = null;
+//                try{
+//                    image = createImage();
+//                }catch(IOException e){
+//                    e.printStackTrace();
+//                }
+//
+//
+//                if (image != null) {
+//                    Uri photoURI = FileProvider.getUriForFile(getContext(), "com.example.install.medstracker.android.fileprovider", image);
+//
+//                    Intent intent = new Intent();
+//                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+//                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+//
+//
+//                    if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+//                        startActivityForResult(intent, CAMERA_INTENT);
+//
+//                    }
+//
+//
+//                }
+            }
+        });
+
+        return view;
+    }
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if(requestCode == CAMERA_INTENT && resultCode == RESULT_OK){
+//           Bitmap bitmapImage = BitmapFactory.decodeFile(imageLocation);
+//            int height = bitmapImage.getHeight() ;
+//            int width = bitmapImage.getWidth();
+//            int newWidth = 400;
+//            int newHeight = newWidth * height / width;
+//            bitmapImage = getResizedBitmap(bitmapImage, newWidth, newHeight);
+//
+////            Matrix matrix = new Matrix();
+////            matrix.postRotate(90);
+////            bitmapImage = Bitmap.createBitmap(bitmapImage, 0, 0, bitmapImage.getWidth(), bitmapImage.getHeight(), matrix, true);
+//
+//            FileOutputStream fOut;
+//            try {
+//                fOut = new FileOutputStream(imageLocation);
+//                bitmapImage.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+//                fOut.flush();
+//                fOut.close();
+//
+//            } catch (FileNotFoundException e1) {
+//                // TODO Auto-generated catch block
+//                e1.printStackTrace();
+//            } catch (IOException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+//
+//
+////            int nh = (int) ( bitmapImage.getHeight() * (512.0 / bitmapImage.getWidth()) );
+////            Bitmap scaled = Bitmap.createScaledBitmap(bitmapImage, 512, nh, true);
+//           // your_imageview.setImageBitmap(scaled);
+//
+//
+//            ImageView imageView = new ImageView(getContext());
+//            imageView.setMaxHeight(400);
+//            imageView.setScaleType(ImageView.ScaleType.FIT_START);
+//            imageView.setAdjustViewBounds(true);
+//            //imageView.setImageBitmap(scaled);
+//            imageView.setImageBitmap(bitmapImage);
+//            imageView.setPadding(0,15,0,15);
+//            galleryLayout.addView(imageView);
+//
+//            Drug book = (Drug) spin.getSelectedItem();
+//           if(book != null) {
+//               Image image = new Image(book.getId(), imageLocation);
+//
+//               /**
+//                * Add the photo to the database
+//                */
+//               DatabaseHandler db = new DatabaseHandler(getContext());
+//               int id = (int) db.addImage(image);
+//               if (id != -1) {
+//                   image.setId(id);
+//                   Toast.makeText(getActivity(), "Photo Added", Toast.LENGTH_LONG).show();
+//               } else {
+//                   Toast.makeText(getActivity(), "Photo Not Added", Toast.LENGTH_LONG).show();
+//               }
+//               db.closeDB();
+//           }else{
+//               Toast.makeText(getActivity(), "Please add a book,\nbefore adding a photo", Toast.LENGTH_LONG).show();
+//           }
+//        }
+//    }
+//
+//    /**
+//     * This method is used to create a temp
+//     * file that the Camera will use to save a photo
+//     * We will generate a file with a collision free name
+//     * (hiking_log_timestamp.jpg)
+//     * This method throws an IOException because we might not
+//     * be able to create the file
+//     * @return
+//     * @throws IOException
+//     */
+//    File createImage() throws IOException{
+//        //Create a timestamp to help create a collision free name
+//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHss").format(new Date());
+//        //Create the name of the image
+//        String fileName = "book_list_" + timeStamp;
+//        //Grab the directory we want to save the image
+//        File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+//        //Create the image in that directory
+//        File bitmapImage = File.createTempFile(fileName, ".jpg", directory);
+//
+//        //Save the location of the image
+//        imageLocation = bitmapImage.getAbsolutePath();
+//        return bitmapImage;
+//    }
+
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        photoPath = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // save file
+            } else {
+                Toast.makeText(getContext().getApplicationContext(), "PERMISSION_DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+//    public File saveBitmapToFile(File file){
+//        try {
+//
+//            // BitmapFactory options to downsize the image
+//            BitmapFactory.Options o = new BitmapFactory.Options();
+//            o.inJustDecodeBounds = true;
+//            o.inSampleSize = 6;
+//            // factor of downsizing the image
+//
+//            FileInputStream inputStream = new FileInputStream(file);
+//            //Bitmap selectedBitmap = null;
+//            BitmapFactory.decodeStream(inputStream, null, o);
+//            inputStream.close();
+//
+//            // The new size we want to scale to
+//            final int REQUIRED_SIZE=75;
+//
+//            // Find the correct scale value. It should be the power of 2.
+//            int scale = 1;
+//            while(o.outWidth / scale / 2 >= REQUIRED_SIZE &&
+//                    o.outHeight / scale / 2 >= REQUIRED_SIZE) {
+//                scale *= 2;
+//            }
+//
+//            BitmapFactory.Options o2 = new BitmapFactory.Options();
+//            o2.inSampleSize = scale;
+//            inputStream = new FileInputStream(file);
+//
+//            Bitmap selectedBitmap = BitmapFactory.decodeStream(inputStream, null, o2);
+//            inputStream.close();
+//
+//            // here i override the original image file
+//            file.createNewFile();
+//            FileOutputStream outputStream = new FileOutputStream(file);
+//
+//            selectedBitmap.compress(Bitmap.CompressFormat.JPEG, 100 , outputStream);
+//
+//            return file;
+//        } catch (Exception e) {
+//            return null;
+//        }
+//    }
+//
+//
+//    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+//        int width = bm.getWidth();
+//        int height = bm.getHeight();
+//        float scaleWidth = ((float) newWidth) / width;
+//        float scaleHeight = ((float) newHeight) / height;
+//        // CREATE A MATRIX FOR THE MANIPULATION
+//        Matrix matrix = new Matrix();
+//        // RESIZE THE BIT MAP
+//        matrix.postScale(scaleWidth, scaleHeight);
+//
+//        // "RECREATE" THE NEW BITMAP
+//        return Bitmap.createBitmap(
+//                bm, 0, 0, width, height, matrix, false);
+//    }
+
+
+
+}
---
+++ MedsTracker2/app/src/main/java/com/example/cdu/medstracker/MainActivity.java
@@ -0,0 +1,185 @@
+package com.example.cdu.medstracker;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener,
+        MainFragment.OnFragmentInteractionListener,
+        AddPhotoFragment.OnFragmentInteractionListener,
+        TakePhotoFragment.OnFragmentInteractionListener,
+        ListViewFragment.OnFragmentInteractionListener,
+        CreateDrugFragment.OnFragmentInteractionListener,
+        ViewPagerControllerFragment.OnFragmentInteractionListener,
+        ViewPagerContentFragment.OnFragmentInteractionListener{
+
+
+    DisplayMetrics displayMetrics = new DisplayMetrics();
+    public static int screenWidth;
+    public static int screenHeight;
+
+    public static FloatingActionButton fab;
+
+    FragmentManager fm = getSupportFragmentManager();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenWidth = displayMetrics.heightPixels;
+        screenHeight = displayMetrics.widthPixels;
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFEA00")));
+        fab.setRippleColor(Color.parseColor("#00E676"));
+
+
+
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.add(R.id.content_main, new MainFragment());
+        transaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+            fab.setImageResource(android.R.drawable.ic_dialog_email);
+            if(fab.isShown()){
+                fab.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            if(fab.isShown()){
+                fab.setVisibility(View.INVISIBLE);
+            }
+            FragmentTransaction transaction = fm.beginTransaction();
+            transaction.setCustomAnimations(R.anim.slide_in, R.anim.slide_out);
+            transaction.addToBackStack(null);
+            transaction.replace(R.id.content_main, new MainFragment());
+            transaction.commit();
+
+        } else if (id == R.id.nav_your_drugs) {
+            FragmentTransaction transaction = fm.beginTransaction();
+            transaction.setCustomAnimations(R.anim.slide_in, R.anim.slide_out);
+            transaction.addToBackStack(null);
+            //transaction.replace(R.id.content_main, new ItemFragment());
+            transaction.replace(R.id.content_main, new ListViewFragment());
+
+            transaction.commit();
+
+        } else if (id == R.id.nav_drugs_guide) {
+
+            FragmentTransaction transaction = fm.beginTransaction();
+            transaction.setCustomAnimations(R.anim.slide_in, R.anim.slide_out);
+            transaction.addToBackStack(null);
+            transaction.replace(R.id.content_main, new ViewPagerControllerFragment());
+            transaction.commit();
+        } else if (id == R.id.nav_add_photo) {
+
+            FragmentTransaction tran = fm.beginTransaction();
+            tran.setCustomAnimations(R.anim.slide_in, R.anim.slide_out);
+            tran.addToBackStack(null);
+            tran.replace(R.id.content_main, new AddPhotoFragment());
+            tran.commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+    public void takePhotos(){
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.content_main, new TakePhotoFragment());
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public void returnToAddPhotos(){
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.content_main, new AddPhotoFragment());
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
