eb314637e8d5834226e4b115adbd7f07534335f5
Michael Dziuba <michael.dziuba@stclairconnect.ca>
2017-04-08 20:48:14 -0400

    Changed PrimaryColor to prevent color change of AppBar, when the app is in suspended state.  Also, edited some content in ViewPager pages.

--- MedsTracker2/app/src/main/java/com/example/cdu/medstracker/MainActivity.java
+++ MedsTracker2/app/src/main/java/com/example/cdu/medstracker/MainActivity.java
@@ -60,15 +60,6 @@ public class MainActivity extends AppCompatActivity implements
         fab.setRippleColor(Color.parseColor("#00E676"));
 
 
-
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -82,13 +73,7 @@ public class MainActivity extends AppCompatActivity implements
         transaction.add(R.id.content_main, new MainFragment());
         transaction.commit();
 
-       // AsyncTask.execute(new Runnable() {
-//            @Override
-//            public void run() {
 
-
-//            }
-//        });
     }
 
     @Override
